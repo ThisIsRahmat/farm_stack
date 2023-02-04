0589ee7979ec31285601701ba997a3ce3fc044d2
thisisrahmat <thisisrahmat@gmail.com>
2023-02-04 07:19:21 +0000

    started chapter 5

---
+++ chap4/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    < h1 className="bg-slate-500 text-white ">
+    This is a Tailwind styled site!
+    </h1>
+
+    </div>
+  );
+}
+
+export default App;
